398a28a5b91bbdc6c23dc43538c9d9794f9deb06
theBashShell <edohbruno@gmail.com>
2019-12-16 18:07:34 +0000

    functions, if, for , etc

--- hello_world/hello-world.go
+++ hello_world/hello-world.go
@@ -1,7 +1,26 @@
 package main
 
-import("fmt")
+import "fmt" 
+
+func add(a, b int) int {
+	return a + b
+}
+
+func swap(a, b string) (string, int) {
+	return a, 0
+}
+
+func some(x string) (y, z int) {
+	y = 10
+	z = 11
+	return
+}
 
 func main() {
-	fmt.Println("Hello World")
+	 sum := 10
+
+	 for sum < 20 {
+		 fmt.Println(sum)
+		 sum++
+	 }
 }
